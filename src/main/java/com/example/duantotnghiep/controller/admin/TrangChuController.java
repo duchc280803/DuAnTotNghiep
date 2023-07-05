6719aa40d68449f0bcb3e408a5390220eae8ef6b
kjuDyHatC <duchc280803@gmail.com>
2023-07-05 23:11:57 +0700

    update

--- src/main/java/com/example/duantotnghiep/controller/admin/TrangChuController.java
+++ src/main/java/com/example/duantotnghiep/controller/admin/TrangChuController.java
@@ -12,4 +12,10 @@ public class TrangChuController {
     public String dashboard(){
         return "/admin/trang-chu";
     }
+
+    @GetMapping("/dashboard/cart")
+    public String banHangTaiQuay(){
+        return "/admin/ban-hang-tai-quay";
+    }
+
 }
