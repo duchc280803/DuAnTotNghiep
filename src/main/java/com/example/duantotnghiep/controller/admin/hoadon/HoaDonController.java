0a32df3e3ad20fea424363f05705a6916a378e7d
DESKTOP-H0QNT1J\ACER <namndph22694@fpt.edu.vn>
2023-07-20 15:23:41 +0700

    giao dien quan li thu chi

--- src/main/java/com/example/duantotnghiep/controller/admin/hoadon/HoaDonController.java
+++ src/main/java/com/example/duantotnghiep/controller/admin/hoadon/HoaDonController.java
@@ -1,4 +1,14 @@
 package com.example.duantotnghiep.controller.admin.hoadon;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/")
 public class HoaDonController {
+    @GetMapping("/dashboard/hoa-don")
+    public String hoadon(){
+        return "/admin/hoaDon/hoa-don";
+    }
 }
