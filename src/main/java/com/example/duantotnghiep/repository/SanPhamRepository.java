057245d1fa5c2ed323ef345ddbaa18a97bdba048
DESKTOP-H0QNT1J\ACER <namndph22694@fpt.edu.vn>
2023-10-09 22:58:38 +0700

    count quantity & calculate the total amount

--- src/main/java/com/example/duantotnghiep/repository/SanPhamRepository.java
+++ src/main/java/com/example/duantotnghiep/repository/SanPhamRepository.java
@@ -1,11 +1,36 @@
 package com.example.duantotnghiep.repository;
 
 import com.example.duantotnghiep.entity.SanPham;
+import com.example.duantotnghiep.mapper.ChiTietSanPhamCustom;
+import com.example.duantotnghiep.mapper.GioHangCustom;
+import com.example.duantotnghiep.mapper.SoLuongGioHangCustom;
+import com.example.duantotnghiep.mapper.TongTienCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface SanPhamRepository extends JpaRepository<SanPham, UUID> {
+    //lấy ra số lượng trên giỏ hàng
+    @Query(value = "SELECT SUM(ghct.soluong) AS soluong\n" +
+            "FROM giohangchitiet ghct\n" +
+            "JOIN giohang gh ON gh.id = ghct.idgiohang\n" +
+            "WHERE gh.id = ?1 AND ghct.soluong > 0;",nativeQuery = true)
+    List<SoLuongGioHangCustom> getSoLuongGioHang(UUID idgh);
+
+    //Tính tổng tiền tất cả sản phẩm trên giỏ hàng
+    @Query(value = "SELECT SUM(sct.giaban * ghct.soluong) AS tongtien\n" +
+            "FROM sanphamchitiet sct\n" +
+            "JOIN sanpham sp ON sct.idsanpham = sp.id\n" +
+            "JOIN size s ON sct.idsize = s.id\n" +
+            "JOIN image img ON sct.id = img.idsanphamchitiet\n" +
+            "JOIN giohangchitiet ghct ON ghct.idsanphamchitiet = sct.id\n" +
+            "JOIN giohang gh ON gh.id = ghct.idgiohang\n" +
+            "JOIN mausac ms ON ms.id = sct.idmausac\n" +
+            "WHERE img.isdefault = 'true' AND gh.id = ?1 AND ghct.soluong > 0;",nativeQuery = true)
+    List<TongTienCustom> getTongTien(UUID idgh);
+
 }
