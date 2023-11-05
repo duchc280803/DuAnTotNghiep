b2ece0e52664ca933ce9864a7e336ae8147c0817
DESKTOP-H0QNT1J\ACER <namndph22694@fpt.edu.vn>
2023-11-05 09:51:17 +0700

    fix where name product and using pathvariable

--- src/main/java/com/example/duantotnghiep/controller/giam_gia_san_pham_controller/SanPhamGiamGiaController.java
+++ src/main/java/com/example/duantotnghiep/controller/giam_gia_san_pham_controller/SanPhamGiamGiaController.java
@@ -8,10 +8,7 @@ import com.example.duantotnghiep.repository.SpGiamGiaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -28,32 +25,32 @@ public class SanPhamGiamGiaController {
         return ResponseEntity.ok(spGiamGiaRepository.getAllSpGiamGia());
     }
 
-    @GetMapping("show-name-price-image")
-    public ResponseEntity<List<loadsanpham_not_login>> getNamePriceImage(@RequestParam UUID idsanpham) {
-        return ResponseEntity.ok(spGiamGiaRepository.getNamePriceImageByIdSanPham(idsanpham));
+    @GetMapping("show-name-price-image/{name}")
+    public ResponseEntity<List<loadsanpham_not_login>> getNamePriceImage(@PathVariable String name) {
+        return ResponseEntity.ok(spGiamGiaRepository.getNamePriceImageByIdSanPham(name));
     }
-    @GetMapping("show-all-mau-sac")
-    public ResponseEntity<List<loadmausac_not_login>> getAllMauSac(@RequestParam UUID idsanpham) {
-        return new ResponseEntity<>(spGiamGiaRepository.getAllMauSac(idsanpham), HttpStatus.OK);
-    }
-
-    @GetMapping("show-all-size")
-    public ResponseEntity<List<loadsize_not_login>> getAllSize(@RequestParam UUID idsanpham) {
-        return new ResponseEntity<>(spGiamGiaRepository.getAllSize(idsanpham), HttpStatus.OK);
+    @GetMapping("show-all-mau-sac/{name}")
+    public ResponseEntity<List<loadmausac_not_login>> getAllMauSac(@PathVariable String name) {
+        return new ResponseEntity<>(spGiamGiaRepository.getAllMauSac(name), HttpStatus.OK);
     }
 
-    @GetMapping("find-size-by-mau-sac")
-    public ResponseEntity<List<loadsize_not_login>> findSizeByMauSac(@RequestParam UUID idsanpham,@RequestParam UUID idmausac) {
-        return new ResponseEntity<>(spGiamGiaRepository.findSizeByMauSac(idsanpham,idmausac), HttpStatus.OK);
+    @GetMapping("show-all-size/{name}")
+    public ResponseEntity<List<loadsize_not_login>> getAllSize(@PathVariable String name) {
+        return new ResponseEntity<>(spGiamGiaRepository.getAllSize(name), HttpStatus.OK);
     }
 
-    @GetMapping("find-mau-sac-by-size")
-    public ResponseEntity<List<loadmausac_not_login>> findMauSacBySize(@RequestParam UUID idsanpham,@RequestParam UUID idsize) {
-        return new ResponseEntity<>(spGiamGiaRepository.findMauSacBySize(idsanpham,idsize), HttpStatus.OK);
+    @GetMapping("find-size-by-mau-sac/{name}")
+    public ResponseEntity<List<loadsize_not_login>> findSizeByMauSac(@PathVariable String name,@RequestParam UUID idmausac) {
+        return new ResponseEntity<>(spGiamGiaRepository.findSizeByMauSac(name,idmausac), HttpStatus.OK);
     }
 
-    @GetMapping("find-idspct-soluong")
-    public ResponseEntity<List<findIdSpctAndSoLuong_not_login>> findIdSpctAndSoLuong_not_login(@RequestParam UUID idmausac, @RequestParam UUID idsize,@RequestParam UUID idsanpham) {
-        return new ResponseEntity<>(spGiamGiaRepository.findIdspctAndSoluong(idmausac,idsize,idsanpham), HttpStatus.OK);
+    @GetMapping("find-mau-sac-by-size/{name}")
+    public ResponseEntity<List<loadmausac_not_login>> findMauSacBySize(@PathVariable String name,@RequestParam UUID idsize) {
+        return new ResponseEntity<>(spGiamGiaRepository.findMauSacBySize(name,idsize), HttpStatus.OK);
+    }
+
+    @GetMapping("find-idspct-soluong/{name}")
+    public ResponseEntity<List<findIdSpctAndSoLuong_not_login>> findIdSpctAndSoLuong_not_login(@RequestParam UUID idmausac, @RequestParam UUID idsize,@PathVariable String name) {
+        return new ResponseEntity<>(spGiamGiaRepository.findIdspctAndSoluong(idmausac,idsize,name), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/duantotnghiep/repository/SpGiamGiaRepository.java
+++ src/main/java/com/example/duantotnghiep/repository/SpGiamGiaRepository.java
@@ -11,7 +11,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,6 +21,7 @@ import java.util.UUID;
 @Repository
 public interface SpGiamGiaRepository extends JpaRepository<SpGiamGia, UUID> {
 
+    //load sanpham on shop
     @Query(value = "SELECT sp.id, i.tenImage, sp.tenSanPham, sp.giaBan, spgg.donGiaKhiGiam, spgg.mucGiam\n" +
             "FROM SanPham sp\n" +
             "LEFT JOIN spgiamgia spgg ON sp.id = spgg.idsanpham\n" +
@@ -42,44 +45,45 @@ public interface SpGiamGiaRepository extends JpaRepository<SpGiamGia, UUID> {
             ")",nativeQuery = true)
     List<loadsanpham_not_login> getAllSpGiamGia();
 
+    //load thong tin san pham detail
     @Query(value = "SELECT sp.id, i.tenImage, sp.tenSanPham, sp.giaBan, spgg.donGiaKhiGiam, spgg.mucGiam\n" +
-            "            FROM SanPham sp\n" +
-            "            LEFT JOIN spgiamgia spgg ON sp.id = spgg.idsanpham\n" +
-            "            LEFT JOIN GiamGia gg ON spgg.idgiamgia = gg.id\n" +
-            "            JOIN Image i ON sp.id = i.idsanpham\n" +
-            "            JOIN ThuongHieu th ON sp.idthuonghieu = th.id\n" +
-            "            JOIN DanhMuc dm ON sp.iddanhmuc = dm.id\n" +
-            "            JOIN XuatXu xx ON sp.idxuatxu = xx.id\n" +
-            "            JOIN kieude kd ON kd.id = sp.idkieude\n" +
-            "            WHERE i.isDefault = 'true' AND sp.id='E4592D5B-6941-4FE0-B067-3E4054207EA8'\n" +
-            "\t\t\tAND (\n" +
-            "    spgg.id IS NULL -- Nếu không có giảm giá nào, hoặc\n" +
-            "    OR (\n" +
-            "        spgg.id IS NOT NULL\n" +
-            "        AND spgg.id = (\n" +
-            "            SELECT MIN(spgg_inner.id)\n" +
-            "            FROM spgiamgia spgg_inner\n" +
-            "            WHERE spgg_inner.idsanpham = sp.id\n" +
-            "        )\n" +
-            "    )\n" +
-            ");\n",nativeQuery = true)
-    List<loadsanpham_not_login> getNamePriceImageByIdSanPham(UUID idsanpham);
+            "                        FROM SanPham sp\n" +
+            "                        LEFT JOIN spgiamgia spgg ON sp.id = spgg.idsanpham\n" +
+            "                        LEFT JOIN GiamGia gg ON spgg.idgiamgia = gg.id\n" +
+            "                        JOIN Image i ON sp.id = i.idsanpham\n" +
+            "                        JOIN ThuongHieu th ON sp.idthuonghieu = th.id\n" +
+            "                        JOIN DanhMuc dm ON sp.iddanhmuc = dm.id\n" +
+            "                        JOIN XuatXu xx ON sp.idxuatxu = xx.id\n" +
+            "                        JOIN kieude kd ON kd.id = sp.idkieude\n" +
+            "                        WHERE i.isDefault = 'true' AND sp.tensanpham=?\n" +
+            "            AND (\n" +
+            "                spgg.id IS NULL \n" +
+            "                OR (\n" +
+            "                    spgg.id IS NOT NULL\n" +
+            "                    AND spgg.id = (\n" +
+            "                        SELECT MIN(spgg_inner.id)\n" +
+            "                        FROM spgiamgia spgg_inner\n" +
+            "                        WHERE spgg_inner.idsanpham = sp.id\n" +
+            "                    )\n" +
+            "                )\n" +
+            "            )",nativeQuery = true)
+    List<loadsanpham_not_login> getNamePriceImageByIdSanPham(String tenSanPham);
 
     //load all mau sac
     @Query(value = "SELECT DISTINCT ms.id, ms.tenmausac\n" +
             "FROM sanpham sp\n" +
             "JOIN sanphamchitiet spct ON sp.id = spct.idsanpham\n" +
             "JOIN mausac ms ON spct.idmausac = ms.id\n" +
-            "WHERE sp.id = ?",nativeQuery = true)
-    List<loadmausac_not_login> getAllMauSac(UUID idsanpham);
+            "WHERE sp.tensanpham = ?",nativeQuery = true)
+    List<loadmausac_not_login> getAllMauSac(String tensanpham);
 
     //load all size
     @Query(value = "SELECT DISTINCT s.id, s.size\n" +
             "FROM sanpham sp\n" +
             "JOIN sanphamchitiet spct ON sp.id = spct.idsanpham\n" +
             "JOIN size s ON spct.idsize = s.id\n" +
-            "WHERE sp.id = ?",nativeQuery = true)
-    List<loadsize_not_login> getAllSize(UUID idsanpham);
+            "WHERE sp.tensanpham = ?",nativeQuery = true)
+    List<loadsize_not_login> getAllSize(String tensanpham);
 
     //find size by mau sac
     @Query(value = "SELECT DISTINCT s.id, s.size\n" +
@@ -87,9 +91,9 @@ public interface SpGiamGiaRepository extends JpaRepository<SpGiamGia, UUID> {
             "JOIN sanphamchitiet spct ON sp.id = spct.idsanpham\n" +
             "JOIN size s ON spct.idsize = s.id\n" +
             "JOIN mausac ms ON spct.idmausac = ms.id\n" +
-            "WHERE sp.id = ?1\n" +
+            "WHERE sp.tensanpham = ?1\n" +
             "AND ms.id = ?2",nativeQuery = true)
-    List<loadsize_not_login> findSizeByMauSac(UUID idsanpham,UUID idmausac);
+    List<loadsize_not_login> findSizeByMauSac(String tensanpham,UUID idmausac);
 
     //find mau sac by size
     @Query(value = "SELECT DISTINCT ms.id, ms.tenmausac\n" +
@@ -97,24 +101,36 @@ public interface SpGiamGiaRepository extends JpaRepository<SpGiamGia, UUID> {
             "JOIN sanphamchitiet spct ON sp.id = spct.idsanpham\n" +
             "JOIN size s ON spct.idsize = s.id\n" +
             "JOIN mausac ms ON spct.idmausac = ms.id\n" +
-            "WHERE sp.id = ?1\n" +
+            "WHERE sp.tensanpham = ?1\n" +
             "AND s.id = ?2",nativeQuery = true)
-    List<loadmausac_not_login> findMauSacBySize(UUID idsanpham,UUID idsize);
+    List<loadmausac_not_login> findMauSacBySize(String tensanpham,UUID idsize);
 
     //load sp chi tiet
-    @Query(value = "SELECT spct.id,soluong FROM SanPham sp\n" +
-            "            LEFT JOIN spgiamgia spgg ON sp.id = spgg.idsanpham\n" +
-            "            LEFT JOIN GiamGia gg ON spgg.idgiamgia = gg.id\n" +
-            "            JOIN Image i ON sp.id = i.idsanpham\n" +
-            "            JOIN ThuongHieu th ON sp.idthuonghieu = th.id\n" +
-            "            JOIN DanhMuc dm ON sp.iddanhmuc = dm.id\n" +
-            "            JOIN XuatXu xx ON sp.idxuatxu = xx.id\n" +
-            "            JOIN sanphamchitiet spct on spct.idsanpham = sp.id\n" +
-            "            JOIN chatlieu cl on spct.idchatlieu = cl.id\n" +
-            "            JOIN mausac ms on spct.idmausac = ms.id\n" +
-            "            JOIN kieude kd on sp.idkieude = kd.id\n" +
-            "            JOIN size s on spct.idsize = s.id\n" +
-            "            WHERE i.isDefault = 'true' and ms.id=?1 and s.id=?2\n" +
-            "            and sp.id=?3",nativeQuery = true)
-    List<findIdSpctAndSoLuong_not_login> findIdspctAndSoluong(UUID idmausac, UUID idsize, UUID idsanpham);
+    @Query(value = "SELECT* FROM SanPham sp\n" +
+            "                        LEFT JOIN spgiamgia spgg ON sp.id = spgg.idsanpham\n" +
+            "                        LEFT JOIN GiamGia gg ON spgg.idgiamgia = gg.id\n" +
+            "                        JOIN Image i ON sp.id = i.idsanpham\n" +
+            "                        JOIN ThuongHieu th ON sp.idthuonghieu = th.id\n" +
+            "                        JOIN DanhMuc dm ON sp.iddanhmuc = dm.id\n" +
+            "                       JOIN XuatXu xx ON sp.idxuatxu = xx.id\n" +
+            "                       JOIN sanphamchitiet spct on spct.idsanpham = sp.id\n" +
+            "                        JOIN chatlieu cl on spct.idchatlieu = cl.id\n" +
+            "                        JOIN mausac ms on spct.idmausac = ms.id\n" +
+            "                        JOIN kieude kd on sp.idkieude = kd.id\n" +
+            "                        JOIN size s on spct.idsize = s.id\n" +
+            "                        WHERE i.isDefault = 'true' and ms.id= :idmausac and s.id= :idsize\n" +
+            "                        and sp.tensanpham = :tensanpham\n" +
+            "\t\t\t\t\t\tand cl.id ='280d4a55-4837-4813-b020-28e3c725f344'\n" +
+            "\t\t\t\t\t\tAND (\n" +
+            "                            spgg.id IS NULL\n" +
+            "                            OR (\n" +
+            "                                spgg.id IS NOT NULL\n" +
+            "                                AND spgg.id = (\n" +
+            "                                    SELECT MIN(spgg_inner.id)\n" +
+            "                                   FROM spgiamgia spgg_inner\n" +
+            "                                   WHERE spgg_inner.idsanpham = sp.id\n" +
+            "                                )\n" +
+            "                            )\n" +
+            "                        )",nativeQuery = true)
+    List<findIdSpctAndSoLuong_not_login> findIdspctAndSoluong(@Param("idmausac") UUID idmausac,@Param("idsize") UUID idsize, @Param("tensanpham") String tensanpham);
 }
