da7f448756ae9fc72253e237691c2ab406a47aa8
kjuDyHatC <duchc280803@gmail.com>
2023-11-08 20:42:53 +0700

    update

--- src/main/java/com/example/duantotnghiep/service/giam_gia_service/GiamGiaService.java
+++ src/main/java/com/example/duantotnghiep/service/giam_gia_service/GiamGiaService.java
@@ -17,8 +17,8 @@ public interface GiamGiaService {
 
     List<ProductDetailResponse> getAllProduct();
 
+    public void updateGiamGia(UUID giamGiaId, GiamGiaRequest updateGiamGiaRequest) ;
     List<GiamGiaResponse> findbyValueString(String key);
-
     List<ProductDetailResponse> findbyProduct(String key);
 
     List<GiamGiaResponse> findbyValueDate(Date key1, Date key2);
@@ -29,7 +29,7 @@ public interface GiamGiaService {
 
     List<ProductDetailResponse> ListSearch(UUID id);
 
-    List<GiamGiaDetailResponse> ListGiamGiaDeatil(UUID id);
+    List<GiamGiaResponse> ListGiamGiaDeatil(UUID id);
 
     MessageResponse createGiamGia(GiamGiaRequest createKhachRequest);
 
