a1d675e82f0038b57efde1152e64fd4dda727399
kjuDyHatC <duchc280803@gmail.com>
2023-12-09 13:48:55 +0700

    update

--- src/main/java/com/example/duantotnghiep/request/TrangThaiHoaDonRequest.java
+++ src/main/java/com/example/duantotnghiep/request/TrangThaiHoaDonRequest.java
@@ -1,5 +1,7 @@
 package com.example.duantotnghiep.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +15,6 @@ public class TrangThaiHoaDonRequest {
 
     private Integer newTrangThai;
 
+    @NotBlank(message = "Không được để trống ghi chú")
     private String ghiChu;
 }
