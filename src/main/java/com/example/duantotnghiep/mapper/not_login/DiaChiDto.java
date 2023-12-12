ca9a976f09bdcef9135fd93752c3287f2c706b88
DESKTOP-H0QNT1J\ACER <namndph22694@fpt.edu.vn>
2023-12-12 14:56:51 +0700

    dia chi quan huyen

--- src/main/java/com/example/duantotnghiep/mapper/not_login/DiaChiDto.java
+++ src/main/java/com/example/duantotnghiep/mapper/not_login/DiaChiDto.java
@@ -6,6 +6,9 @@ import lombok.Data;
 public class DiaChiDto {
         private String fullName;
         private String soDienThoai;
+        private String tinh;
+        private String huyen;
+        private String xa;
         private String diaChi;
         private String trangThai;
 }
