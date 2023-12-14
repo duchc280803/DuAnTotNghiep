2484f05e6fd8f2388b73ab71d3dd273764850acd
DESKTOP-H0QNT1J\ACER <namndph22694@fpt.edu.vn>
2023-12-14 23:41:33 +0700

    giohang

--- src/main/java/com/example/duantotnghiep/service/mua_hang_not_login_impl/GioHangServiceImpl_not_login.java
+++ src/main/java/com/example/duantotnghiep/service/mua_hang_not_login_impl/GioHangServiceImpl_not_login.java
@@ -1,12 +1,19 @@
 package com.example.duantotnghiep.service.mua_hang_not_login_impl;
 
 import com.example.duantotnghiep.entity.GioHang;
+import com.example.duantotnghiep.entity.TaiKhoan;
+import com.example.duantotnghiep.repository.TaiKhoanRepository;
 import com.example.duantotnghiep.repository.mua_hang_not_login_repo.GioHangRepository_not_login;
 import com.example.duantotnghiep.service.mua_hang_not_login_service.GioHangService_not_login;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.sql.Date;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -14,6 +21,9 @@ public class GioHangServiceImpl_not_login implements GioHangService_not_login {
 
     @Autowired
     private GioHangRepository_not_login gioHangRepository;
+
+    @Autowired
+    private TaiKhoanRepository taiKhoanRepository;
 
 
     public UUID taoGioHang() {
@@ -26,4 +36,30 @@ public class GioHangServiceImpl_not_login implements GioHangService_not_login {
         return gioHangMoi.getId();
     }
 
+    public UUID taoGioHangLogin(Principal principal) {
+        GioHang gioHang = new GioHang();
+        gioHang.setId(UUID.randomUUID());
+        gioHang.setNgayTao(new Date(System.currentTimeMillis()));
+        gioHang.setTrangThai(1);
+
+        Optional<TaiKhoan> taiKhoan = taiKhoanRepository.findByUsername(principal.getName());
+
+        gioHang.setTaiKhoan(taiKhoan.get());
+        GioHang gioHangMoi = gioHangRepository.save(gioHang);
+        return gioHangMoi.getId();
+    }
+
+
+    public UUID getGioHangLogin(Principal principal) {
+            String username = principal.getName();
+
+            UUID gioHangId = gioHangRepository.getGioHangLogin(username);
+
+            // Kiểm tra xem giỏ hàng có tồn tại không
+            if (gioHangId != null) {
+                return gioHangId;
+            } else {
+                return null;
+            }
+    }
 }
