eca000e63f5539d9cbfe9a703771b2f6f1dee7f6
HieuNM151 <hieunm151@gmail.com>
2023-12-19 15:33:32 +0700

    no message

--- src/main/java/com/example/duantotnghiep/controller/hoa_don_controller/HoaDonChiTietController.java
+++ src/main/java/com/example/duantotnghiep/controller/hoa_don_controller/HoaDonChiTietController.java
@@ -35,8 +35,12 @@ public class HoaDonChiTietController {
     }
 
     @GetMapping("hien-thi-san-pham/{idHoaDon}")
-    public ResponseEntity<List<SanPhamHoaDonChiTietResponse>> getSanPhamHDCT(@PathVariable(name = "idHoaDon") UUID idHoaDon) {
-        return new ResponseEntity<>(hoaDonChiTietService.getSanPhamHDCT(idHoaDon), HttpStatus.OK);
+    public ResponseEntity<List<SanPhamHoaDonChiTietResponse>> getSanPhamHDCT(
+            @PathVariable(name = "idHoaDon") UUID idHoaDon,
+            @RequestParam(name = "pageNumber", defaultValue = "0") Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = "5") Integer pageSize
+) {
+        return new ResponseEntity<>(hoaDonChiTietService.getSanPhamHDCT(pageNumber, pageSize, idHoaDon), HttpStatus.OK);
     }
 
     @GetMapping("hien-thi-lich-su/{idHoaDon}")
